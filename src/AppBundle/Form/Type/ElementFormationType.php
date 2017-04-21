b6ae6ca87250870be5e53fcd54df21d299f1ac84
Allan ELLEUCH <allan.elleuch@gmail.com>
2017-04-21 14:02:12 +0200

    Amélioration du visuel du formulaire de cursus

--- src/AppBundle/Form/Type/ElementFormationType.php
+++ src/AppBundle/Form/Type/ElementFormationType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use AppBundle\Entity\ElementFormation;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 class ElementFormationType extends AbstractType
 {
@@ -29,17 +30,14 @@ public function buildForm(FormBuilderInterface $builder, array $options)
 {
   $builder
   ->add('sigle', TextType::class, array('label' => 'Sigle ', 'attr' => array('placeholder' => 'sigle d’une UE, label d’un stage,', 'class' => 'form-control')))
-  ->add('credit', TextType::class, array('label' => 'credit ', 'attr' => array('placeholder' => 'nombre de crédits obtenus', 'class' => 'form-control')))
-  ->add('sem_seq', TextType::class, array('label' => 'numéro semestre ', 'attr' => array('placeholder' => 'numéro de semestre à l’UTT', 'class' => 'form-control')))
-  ->add('sem_label', TextType::class, array('label' => 'label du semestre ', 'attr' => array('TC1, ... TC6, ISI1 ... ISI8, SRT1,..., MTE, ....', 'class' => 'form-control')))
-  ->add('utt', CheckboxType::class, array('label' => 'utt','required' => false, 'attr' => array('TC1, ... TC6, ISI1 ... ISI8, SRT1,..., MTE, ....', 'class' => 'form-control')))
+  ->add('credit', IntegerType ::class, array('label' => 'credit ', 'attr' => array('placeholder' => 'nombre de crédits obtenus', 'class' => 'form-control', 'min' => '0')))
+  ->add('sem_seq', IntegerType ::class, array('label' => 'numéro semestre ', 'attr' => array('placeholder' => 'numéro de semestre à l’UTT', 'class' => 'form-control', 'min' => '0')))
+  ->add('sem_label', TextType::class, array('label' => 'label du semestre ', 'attr' => array('placeholder' =>'TC1, ... TC6, ISI1 ... ISI8, SRT1,..., MTE, ....', 'class' => 'form-control ')))
   ->add('affectations', EntityType::class, array(    'class' => 'AppBundle:Affectations',    'choice_label' => 'label'))
   ->add('categories', EntityType::class, array(    'class' => 'AppBundle:Categories',    'choice_label' => 'label'))
   ->add('resultats', EntityType::class, array(    'class' => 'AppBundle:Resultats',    'choice_label' => 'label'))
-
-
-
-  ->add('profil', CheckboxType::class, array('label' => 'profil','required' => false, 'attr' => array('TC1, ... TC6, ISI1 ... ISI8, SRT1,..., MTE, ....', 'class' => 'form-control','title' => 'test')));
+  ->add('utt', CheckboxType::class, array('label' => "UV prise à l'utt",'required' => false, 'attr' => array( 'class' => 'form-control')))
+  ->add('profil', CheckboxType::class, array('label' => 'UV de profil','required' => false, 'attr' => array('class' => 'form-control','title' => 'test')));
 
 
 }
