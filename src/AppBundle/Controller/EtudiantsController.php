33f9213780ccdf21e9cea5fb96553d8d8695857c
Corentin Laithier <corentinlaithier.89@gmail.com>
2017-05-04 08:52:41 +0200

    Add views and controller for Etudiants

--- src/AppBundle/Controller/EtudiantsController.php
+++ src/AppBundle/Controller/EtudiantsController.php
@@ -7,8 +7,34 @@
  */
 
 namespace AppBundle\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
+class EtudiantsController extends Controller{
+    /**
+     * affiche les cursus d'un étudiant
+     * @Route("/etudiants/etudiants/")
+     */
+    public function viewStudentsAction(Request $request) {
 
-class EtudiantsController {
+        return $this->render('etudiants/etudiants.html.twig', array(
+            'nav' => "etudiants",
+            'subnav' => 'etudiants',
+        ));
 
+    }
+
+    /**
+     * affiche les cursus d'un étudiant
+     * @Route("/etudiants/new/")
+     */
+    public function newStudentAction(Request $request) {
+
+        return $this->render('etudiants/etudiants.html.twig', array(
+            'nav' => "etudiants",
+            'subnav' => 'new',
+        ));
+
+    }
 }
