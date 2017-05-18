52cbbcd582eb9ff0bb909ed3dfaf6daa4ce3a4f7
Corentin Laithier <corentinlaithier.89@gmail.com>
2017-05-18 10:55:17 +0200

    Add reglement export to csv (contains issue to fix)

--- src/AppBundle/Controller/ReglementsController.php
+++ src/AppBundle/Controller/ReglementsController.php
@@ -10,6 +10,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 
 class ReglementsController extends Controller {
@@ -39,6 +41,84 @@ class ReglementsController extends Controller {
             'subnav' => "import",
         ));
     }
+
+
+
+    /**
+     * @Route("/reglements/export/{id}")
+     */
+    public function exportOneReglementAction(Request $request, $id) {
+
+        /* Récupération de l'objet cursus */
+        $reglement = $this->getDoctrine()
+            ->getRepository('AppBundle:Reglement')
+            ->find($id);
+
+        /* Si le chemin d'exportation n'existe pas, on le crée.
+         * S'il existe déjà, on le vide pour ne pas stocker sur le serveur tous les fichiers exportés.
+         * Seul le dernier fichier exporté est gardé sur le serveur.
+         */
+        if (!is_dir('./export_reglements')){
+            mkdir('./export_reglements');
+        } else {
+            $files = glob('./export_reglements/*');
+            foreach ($files as $file) {
+                if (is_file($file)){
+                    unlink($file);
+                }
+            }
+        }
+
+        /* Variables d'accès et d'ouverture du fichier csv */
+        $filename = $reglement->getId() . "_" . $reglement->getLabel() . ".csv";
+        $csvFilePath = "./export_reglements/" . $filename;
+        $csvFile = fopen($csvFilePath, 'w');
+
+
+        /* Écriture de la ligne intermédiaire */
+        $headLine = array(
+            "LABEL",
+            $reglement->getLabel() ,
+            "",
+            "",
+            ""
+        );
+        fputcsv($csvFile, $headLine, ";");
+
+        $rules = $reglement->getRegles();
+
+        foreach ($rules as $rule){
+            $line = array(
+                "R" . $rule->getId(),
+                $this->getDoctrine()
+                    ->getRepository('AppBundle:Agregat')
+                    ->find($rule->getAgregat())->getLabel(),
+                $rule->getCibleAgregat(),
+                /* BEGIN : ISSUE HERE */
+                $this->getDoctrine()
+                    ->getRepository('AppBundle:Affectations')
+                    ->find($rule->getAffectations())->getLabel(),
+                /* END : ISSUE HERE */
+                $rule->getSeuil(),
+            );
+            fputcsv($csvFile, $line, ";");
+        }
+        fclose($csvFile);
+
+
+        // Téléchargement automatique du fichier
+        $response = new BinaryFileResponse($csvFilePath);
+        $response->headers->set('Content-Type', 'text/plain');
+        $response->setContentDisposition(
+            ResponseHeaderBag::DISPOSITION_ATTACHMENT
+        );
+
+        return $response;
+
+    }
+
+
+
     /**
      * @Route("/reglements/export/")
      */
